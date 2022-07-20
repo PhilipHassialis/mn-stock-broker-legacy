bc8922b1f1f2d729cefc9712528807a5b5b377ec
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-07-20 08:37:42 +0300

    Custom HttpResponse

--- src/main/java/com/hassialis/philip/broker/QuotesController.java
+++ src/main/java/com/hassialis/philip/broker/QuotesController.java
@@ -2,10 +2,12 @@ package com.hassialis.philip.broker;
 
 import java.util.Optional;
 
+import com.hassialis.philip.broker.error.CustomError;
 import com.hassialis.philip.broker.model.Quote;
 import com.hassialis.philip.broker.store.InMemoryStore;
 
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
@@ -22,6 +24,15 @@ public class QuotesController {
   @Get("/{symbol}")
   public HttpResponse getQuote(@PathVariable String symbol) {
     final Optional<Quote> maybeQuote = store.fetchQuote(symbol);
+    if (maybeQuote.isEmpty()) {
+      final CustomError notFound = CustomError.builder()
+          .status(HttpStatus.NOT_FOUND.getCode())
+          .error(HttpStatus.NOT_FOUND.name())
+          .message("Quote for symbol not available")
+          .path("/quotes/" + symbol)
+          .build();
+      return HttpResponse.notFound(notFound);
+    }
     return HttpResponse.ok(maybeQuote.get());
 
   }
--- src/test/java/com/hassialis/philip/broker/QuotesControllerTest.java
+++ src/test/java/com/hassialis/philip/broker/QuotesControllerTest.java
@@ -1,10 +1,11 @@
 package com.hassialis.philip.broker;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.inject.Inject;
@@ -13,16 +14,17 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.hassialis.philip.broker.error.CustomError;
 import com.hassialis.philip.broker.model.Quote;
 import com.hassialis.philip.broker.model.Symbol;
 import com.hassialis.philip.broker.store.InMemoryStore;
 
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @MicronautTest
 public class QuotesControllerTest {
@@ -55,6 +57,22 @@ public class QuotesControllerTest {
     final Quote amznResult = client.toBlocking().retrieve("/quotes/AMZN", Quote.class);
     LOG.debug("Result {}", amznResult);
     assertThat(amznQuote).usingRecursiveComparison().isEqualTo(amznResult);
+  }
+
+  @Test
+  void returnsNotFoundOnUnsupportedSymbol() {
+    try {
+      client.toBlocking().retrieve("/quotes/UNSUPPORTED", Quote.class, CustomError.class);
+    } catch (HttpClientResponseException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getResponse().getStatus());
+      LOG.debug("Body: {}", e.getResponse().getBody());
+      final Optional<CustomError> customError = e.getResponse().getBody(CustomError.class);
+      assertTrue(customError.isPresent());
+      assertEquals(404, customError.get().getStatus());
+      assertEquals("NOT_FOUND", customError.get().getError());
+      assertEquals("Quote for symbol not available", customError.get().getMessage());
+      assertEquals("/quotes/UNSUPPORTED", customError.get().getPath());
+    }
   }
 
   private Quote initRandomQuote(String symbolName) {
